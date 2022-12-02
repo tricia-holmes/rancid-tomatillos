75e8aae04069f6cf1c6cd6eed09d92cf2dff9ce2
Dani Bagley <bagley.a.danielle@gmail.com>
2022-12-02 15:18:38 -0700

    Add render method and fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      currentMovie: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({ movies: data.movies });
+      });
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Racid Tomatillos</h1>
+        {/* <Banner />
+        <Movies />
+        {this.state.currentMovie && <MovieDetails />} */}
+      </main>
+    );
+  }
 }
 
 export default App;

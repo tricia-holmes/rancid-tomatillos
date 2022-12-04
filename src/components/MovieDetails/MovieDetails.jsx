1c877e84b2eaec38cde5d1dbac611bdf3c59868b
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-12-04 14:51:17 -0600

    Add initial JSX elements for modal to display current movie details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react'
+import './MovieDetails.css'
 
-const MovieDetails = () => {
-  return <h1>Placeholder for movie details</h1>;
-};
+const MovieDetails = ({ currentMovie }) => {
+  return (
+    <div className="movieDetailsModal">
+      <div className="movieDetailsContent">
+        <img className='movieDetailsPoster' src={`${currentMovie['poster_path']}`} alt="" />
+        <h1 className='movieDetailsTitle'>{currentMovie.title}</h1>
+        <div className='movieDetailsInfo'>
+          <p>Average Rating: {Math.round(currentMovie['average_rating'] * 10) / 10}</p>
+          <p>Release Date: {currentMovie['release_date']}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default MovieDetails;
+export default MovieDetails

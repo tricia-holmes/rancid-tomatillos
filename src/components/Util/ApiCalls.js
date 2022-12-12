32d6864c3028a807d8dc58d3de88b7898e2a5090
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-12-12 10:08:42 -0600

    Add timeouts to loading and erros in API calls

--- src/components/Util/ApiCalls.js
+++ src/components/Util/ApiCalls.js
@@ -1,40 +1,43 @@
-export const loadData = (URL, item) => {
-  fetch(URL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("weird error");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      item.setState({ movies: data.movies });
-      item.setState({ filteredMovies: data.movies });
-      item.setState({ loading: false });
-      const helperArray = [];
-      data.movies.forEach((movie) => {
-        getSingleMovie(URL, movie.id, helperArray, item);
-      });
-      item.setState({ moviesWithDetails: helperArray });
-    })
-    .catch((err) => {
-      item.setState({ loading: false });
-      item.setState({ hasError: true });
-    });
-};
 
 const getSingleMovie = (URL, id, array, thing) => {
   fetch(`${URL}/${id}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("SINGLE ERROR");
-      }
-      return response.json();
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("SINGLE ERROR");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    array.push(data.movie);
     })
-    .then((data) => {
-      array.push(data.movie);
-    })
-    .catch((err) => {
-      thing.setState({ loading: false });
-      thing.setState({ hasError: true });
-    });
-};
+  };
+  
+  export const loadData = (URL, item) => {
+    fetch(URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("weird error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        item.setState({ movies: data.movies });
+        item.setState({ filteredMovies: data.movies });
+        const helperArray = [];
+        data.movies.forEach((movie) => {
+          getSingleMovie(URL, movie.id, helperArray, item);
+        });
+        item.setState({ moviesWithDetails: helperArray });
+        setTimeout(() => {
+          item.setState({ loading: false });
+        }, 2000)
+      })
+      .catch((err) => {
+        setTimeout(() => {
+          item.setState({ loading: false });
+          item.setState({ hasError: true });
+        }, 2000)
+      });
+  };
+
+

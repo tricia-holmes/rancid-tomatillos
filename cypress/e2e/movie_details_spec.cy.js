4ab394f122cb83997d13485eedfc2ccb9f957d6e
Zac Walters <zacwalters4@gmail.com>
2022-12-12 18:33:56 -0700

    Refactor movie detail tests to reflect new movie detail data

--- cypress/e2e/movie_details_spec.cy.js
+++ cypress/e2e/movie_details_spec.cy.js
@@ -1,28 +1,27 @@
 describe('Individual movie page testing', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    // cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'movie_data.json' })
   })
-  it('should route to the movie id url when clicked', () => {
+  it.skip('should route to the movie id url when clicked', () => {
     cy.get('.bannerImages').get('div')
-      .get('img[id="539885"]').click()
-      .url().should('include', '/539885')
+      .get('img[id="830784"]').click()
+      .url().should('include', '/830784')
   })
 
   it('should show additional details about movie when clicked', () => {
     cy.get('.bannerImages').get('div')
-      .get('img[id="413518"]').click()
-      .get('.movieDetailsContent')
-        .find('div')
-        .should('have.class', 'movieDetailsInfo')
-        .contains('Average Rating:')
-        .contains('Release Date:')
+      .get('img[id="820067"]').click()
+    cy.contains('THE QUINTESSENTIAL QUINTUPLETS MOVIE')
+      .get('h3')
+      .contains('When five lovely young girls who hate studying hire part-time tutor Futaro, he guides not only their education but also their hearts. Time spent has brought them all closer, with feelings growing within the girls and Futaro. As they finish their third year of high school and their last school festival approaches, they set their sights on what’s next. Is there a future with one of them and Futaro?')
+      .get('img')
+        .should('have.attr', 'alt')
   })
 
   it('should route back to the home page when back is clicked', () => {
     cy.get('.bannerImages').get('div')
-      .get('img[id="726739"]').click()
-      .url().should('include', '/726739')
+      .get('img[id="505642"]').click()
+      .url().should('include', '/505642')
       .get('button').click()
       .url().should('include', '/')
   })
